c37df9d59a7c078ec9ba27802cff70ef5a2ca66a
Ken Vu <ken@gladeye.co.nz>
2017-06-16 11:44:50 +1200

    Move the `dev-server` config into a block

---
+++ build/blocks/dev-server.js
@@ -0,0 +1,45 @@
+const { config } = require('../utils'),
+    { argv } = require('yargs'),
+    respMod = require('resp-modifier'),
+    webpack = require('webpack'),
+    FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin'),
+    PHP_URL = process.env.PHP_URL;
+
+module.exports = config(function(instance) {
+    return instance.merge({
+        output: {
+            pathinfo: true
+        },
+        devServer: {
+            disableHostCheck: true,
+            historyApiFallback: true,
+            noInfo: true,
+            compress: false,
+            hot: true,
+            inline: true,
+            quiet: true,
+            overlay: true,
+            proxy: {
+                '/': {
+                    target: PHP_URL,
+                    changeOrigin: true,
+                    autoRewrite: true
+                }
+            },
+            setup(app) {
+                app.use(respMod({
+                    rules: [
+                        {
+                            match: new RegExp(PHP_URL.replace('//', '\/\/'), 'gi'),
+                            replace: `http://localhost:${argv.port}`
+                        }
+                    ]
+                }));
+            }
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new FriendlyErrorsWebpackPlugin()
+        ]
+    });
+});
--- build/webpack/serve.config.js
+++ build/webpack/serve.config.js
@@ -1,47 +1,8 @@
-const { config } = require('../utils'),
-    { argv } = require('yargs'),
-    respMod = require('resp-modifier'),
-    webpack = require('webpack'),
-    FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin'),
-    PHP_URL = process.env.PHP_URL;
+const { config } = require('../utils');
 
 module.exports = config(function(instance) {
-    return instance
-    .extend('build/webpack/build.config.js')
-    .merge({
-        output: {
-            pathinfo: true
-        },
-        devServer: {
-            disableHostCheck: true,
-            historyApiFallback: true,
-            noInfo: true,
-            compress: false,
-            hot: true,
-            inline: true,
-            quiet: true,
-            overlay: true,
-            proxy: {
-                '/': {
-                    target: PHP_URL,
-                    changeOrigin: true,
-                    autoRewrite: true
-                }
-            },
-            setup(app) {
-                app.use(respMod({
-                    rules: [
-                        {
-                            match: new RegExp(PHP_URL.replace('//', '\/\/'), 'gi'),
-                            replace: `http://localhost:${argv.port}`
-                        }
-                    ]
-                }));
-            }
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin(),
-            new FriendlyErrorsWebpackPlugin()
-        ]
-    });
+    return instance.extend(
+        'build/webpack/build.config.js',
+        'build/blocks/dev-server.js'
+    );
 });
