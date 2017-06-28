25df3f2b140d1a586b10aa5943422a2eb3f8e7ce
Benjamin Sharp <benjamin.sharp@gladeye.co.nz>
2017-06-28 16:43:42 +1200

    Implement post-type and tax register lists

--- public/content/themes/sage/resources/functions.php
+++ public/content/themes/sage/resources/functions.php
@@ -1,3 +1,13 @@
 <?php
 // Initialise Sage theme
 require_once(ABSPATH . "../../vendor/roots/sage/resources/functions.php");
+
+/**
+ * Include custom post types definitions
+ */
+require_once(ABSPATH ."../content/themes/sage/app/post-types.php");
+
+/**
+ * Include custom Taxonomies definitions
+ */
+require_once(ABSPATH ."../content/themes/sage/app/taxonomies.php");
